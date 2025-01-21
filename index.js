ea1ff2d728b3910ee74a664f17f3f92014f7f841
Anton_Yashchenko <151663096+AntoXa32@users.noreply.github.com>
2025-01-21 17:51:43 +0200

    add form

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
-const lovedBtnEl = document.querySelector(".loved-btn");
-const modalCloseBtnEl = document.querySelector(".modal-close-btn");
-const backdropEl = document.querySelector(".backdrop");
+const elements = {
+  lovedBtnEl: document.querySelector(".loved-btn"),
+  modalCloseBtnEl: document.querySelector(".modal-close-btn"),
+  backdropEl: document.querySelector(".backdrop"),
 
-const menuOpenBtnEl = document.querySelector(".menu-open-btn");
-const menuCloseBtnEl = document.querySelector(".mob-close-btn");
-const mobMenuEl = document.querySelector(".mob-menu");
+  menuOpenBtnEl: document.querySelector(".menu-open-btn"),
+  menuCloseBtnEl: document.querySelector(".mob-close-btn"),
+  mobMenuEl: document.querySelector(".mob-menu"),
+  loginForm: document.querySelector(".modal-form"),
+  checkBoxEl: document.querySelector("#policy"),
+  submitBtnEl: document.querySelector(".modal-btn"),
+};
 
-lovedBtnEl.addEventListener("click", () => {
-  backdropEl.classList.add("is-open");
+// Відкриття/закриття модалки
+elements.lovedBtnEl.addEventListener("click", () => {
+  elements.backdropEl.classList.add("is-open");
 });
 
-modalCloseBtnEl.addEventListener("click", () => {
-  backdropEl.classList.remove("is-open");
+elements.modalCloseBtnEl.addEventListener("click", () => {
+  elements.backdropEl.classList.remove("is-open");
 });
 
-menuOpenBtnEl.addEventListener("click", () => {
-  mobMenuEl.classList.add("is-open");
+// Відкриття/закриття мобільного меню
+elements.menuOpenBtnEl.addEventListener("click", () => {
+  elements.mobMenuEl.classList.add("is-open");
 });
 
-menuCloseBtnEl.addEventListener("click", () => {
-  mobMenuEl.classList.remove("is-open");
+elements.menuCloseBtnEl.addEventListener("click", () => {
+  elements.mobMenuEl.classList.remove("is-open");
+});
+
+elements.checkBoxEl.addEventListener("input", () => {
+  elements.submitBtnEl.disabled = !elements.checkBoxEl.checked;
+});
+
+elements.loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = {
+    name: this.elements.userName.value.trim(),
+    email: this.elements.userEmail.value.trim(),
+    tel: this.elements.userTel.value.trim(),
+    comment: this.elements.userComment.value.trim(),
+  };
+
+  if (Object.values(formData).some((value) => value === "")) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  if (!elements.checkBoxEl.checked) {
+    alert(
+      "You must agree to the terms and conditions before submitting the form."
+    );
+    return;
+  }
+
+  console.log(formData);
+
+  this.reset();
+  elements.submitBtnEl.disabled = true;
 });
